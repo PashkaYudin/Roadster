f1b9b992d2070168a27159119e3e3e4ab1a72b73
PashkaYudin <coolbatmanarrow@gmail.com>
2016-03-27 18:10:08 +0600

    Add a Map and intent to NewEventActivity. It's OK

--- app/src/main/java/com/example/broso/roadster/MainActivity.java
+++ app/src/main/java/com/example/broso/roadster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.broso.roadster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,11 +23,20 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                newEvent();
             }
         });
     }
+
+
+    public void newEvent() {
+        Intent newEvent = new Intent(this, NewEvent.class);
+        startActivityForResult(newEvent, 1);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
